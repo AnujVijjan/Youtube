53b7ee1c300a9500a2c819325860a13ad394dec2
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-06-11 19:14:44 +0530

    Create tutorial15.py

---
+++ tutorial15.py
@@ -0,0 +1,48 @@
+# Arithmetic Operators
+# print("10 + 3 is ", 10 + 3)
+# print("10 - 3 is ", 10 - 3)
+# print("10 * 3 is ", 10 * 3)
+# print("10 / 3 is ", 10 / 3)
+# print("10 // 3 is ", 10 // 3)
+# print("10 ** 3 is ", 10 ** 3)
+# print("10 % 3 is ", 10 % 3)
+
+# Comparison Operators
+# print("5 > 3 ", 5 > 3)
+# print("5 < 3 ", 5 < 3)
+# print("5 >= 6 ", 5 >= 6)
+# print("5 <= 3 ", 5 <= 3)
+# print("5 != 3 ", 5 != 3)
+# print("5 == 5 ", 5 == 5)
+
+# Assignment Operators
+# a = 10
+# b = 20
+
+# # a += b # a = a + b
+# # a -= b # a = a - b
+# # a *= b # a = a * b
+# # a /= b # a = a / b
+# a %= b 
+
+# print(a)
+# Logical Operators
+# print(not 3 > 5)
+# Bitwise Operators
+# 00 0 
+# 01 1
+# 10 1
+# 11 0
+
+# print(1 ^ 1)
+# Membership Operators
+# print(10 in "102030")
+# Identity Operators
+
+a = 10
+b = 30
+
+print(id(a))
+print(id(b))
+print(a is not b)
+
