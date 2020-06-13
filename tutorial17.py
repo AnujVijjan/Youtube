4988231571138214e7ea23a7b14cb762c6dae619
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-06-13 18:48:41 +0530

    Create tutorial17.py

---
+++ tutorial17.py
@@ -0,0 +1,30 @@
+# String
+a = "Python Programming"
+
+# # Slice Constructor
+# sub = slice(0, 6, 2)
+
+# # Using indexing sequence 
+
+# print(a[-5 : -2 : 2])
+
+l = [10, 20 , 30 , 40, 50]
+
+# sub = slice(-3, -1, 2)
+
+# # Using indexing sequence 
+# print(l[-3])
+
+t = (10, 20, 30, 40, 50)
+
+# Slice Constructor
+sub = slice(-4, -1)
+
+# Using indexing sequence 
+# print(t[-5:])
+
+print(t[::-1])
+
+print(a[::-1])
+
+print(l[::-1])
