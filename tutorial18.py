c63555f72eca6783cba4412f224ca792cdc32b0d
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-06-14 20:58:45 +0530

    Create tutorial18.py

---
+++ tutorial18.py
@@ -0,0 +1,33 @@
+# If statement
+# a = 10
+# b = 5
+# if( a > b):
+#     print("a is greater than b")
+
+# If-else statement
+# a = 5
+# b = 10
+# if( a > b ):
+#     print("a is greater than b")
+# else:
+#     print("b is greater than a")
+
+# If statement
+# a = 10
+# b = 10
+# if( a > b ):
+#     print("a is greater than b")
+# elif( b > a ):
+#     print("b is greater than a")
+# else:
+#     print("both are equal")
+
+a = 6
+
+
+if( a > 5 ):
+    print("Above 5, ", end="")
+    if(a > 10):
+        print("also above 10")
+    else:
+        print("but not above 10")
