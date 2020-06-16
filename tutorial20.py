87da10814c0be6bd6d4363266bbe69a767730f41
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-06-16 19:11:06 +0530

    Create tutorial20.py

---
+++ tutorial20.py
@@ -0,0 +1,24 @@
+# for item in [10, 20, 30, 40]:
+#     print(item)
+
+# for item in "Python":
+#     print(item)
+
+
+# for i in range(2, 7, 3):
+#     print(i)
+
+# print(type(range(7)))
+
+# for i in [10, 20, 30, 40]:
+#     print(i)
+# else:
+#     print('Finally Finished!')
+
+
+# for i in range(6):
+#     for j in range(2):
+#         print(i , j)
+
+for _ in range(10):
+    print("Hello World")
