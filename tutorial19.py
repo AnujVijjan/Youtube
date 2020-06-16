173f1a6986eda162a68b56285ac5f93b7c3df0e3
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-06-16 19:09:49 +0530

    Create tutorial19.py

---
+++ tutorial19.py
@@ -0,0 +1,20 @@
+# running = True
+# while(running):
+#     print("Hello world ", i)
+#     running = False
+# else:
+#     print("Program Execution Ended!")
+
+# i = 0
+# while(i < 10):
+#     print("Hello world ", i)
+#     i += 1
+# else:
+#     print("Program Execution Ended!")
+
+i = 10
+while i > 0:
+    print("Hello world ", i)
+    i -= 1
+else:
+    print("Program Execution Ended!")
