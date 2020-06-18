3b0090e9f5b5dd0583ff78851f22c16d9aa8a6fd
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-06-18 19:58:01 +0530

    Create tutorial22.py

---
+++ tutorial22.py
@@ -0,0 +1,17 @@
+# for i in range(5):
+#     print(i)
+#     if(i == 3):
+#         break
+    
+# for i in range(5):
+#     if(i == 2):
+#         continue
+#     print(i)
+
+# for i in range(10):
+#     pass
+
+for i in range(5):
+    print(i)
+else:
+    print("For loop Ended")
