7e54b482ca1f0181dba1adba41f6499ceff3704d
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-06-21 21:32:04 +0530

    Create tutorial25.py

---
+++ tutorial25.py
@@ -0,0 +1,50 @@
+# def my_function():
+#     print("Hello from a function")
+
+# def my_function():
+#     print("Hello from a function")
+#     print("*")
+
+
+# def my_function(name):
+#     print("Name = " + name)
+#     print("*")
+
+
+# def my_function(fname, lname="Vijjan"):
+#     print("First Name = " + fname)
+#     print("LAst Name = " + lname)
+
+# my_function("anuj")
+
+# def my_func(myList):
+#     for i in myList:
+#         print(i)
+
+# my_func([10,20,30,40])
+
+# i = 5
+# def my_func(a = i):
+#     print(a)
+
+# i = 6
+# my_func()
+
+# def my_func(a , myList = []):
+#     myList.append(a)
+#     print(myList)
+
+# my_func(10)
+# my_func(20)
+# my_func(30)
+
+
+# def my_func(no):
+#     return no * 5
+
+# print(my_func(10))
+
+def my_func():
+    return
+
+my_func()
