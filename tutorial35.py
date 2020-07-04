24470ddb5e34d136283457e4b45ddb196461ab21
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-07-04 19:50:55 +0530

    Create tutorial35.py

---
+++ tutorial35.py
@@ -0,0 +1,22 @@
+# a = 30
+# l = 20
+def demo():
+    global l
+    # l = 20
+
+    # a = 20
+    # a = a + 10
+    print(l)
+    # def demo1():
+    #     print(l)
+    
+    # demo1()
+    
+# print(l)
+
+
+demo()
+# print(a)
+print(l)
+
+
