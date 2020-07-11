9d4bb12eb5c6cb9fcd0f81618d1c1344c450d687
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-07-12 01:20:40 +0530

    Create tutorial41.py

---
+++ tutorial41.py
@@ -0,0 +1,25 @@
+# tutorial41.py 
+
+# import Game
+# import Game.main
+
+
+# print(Game.a)
+
+# print(Game.main.b)
+
+# import Game.Level.start
+# from Game.Level import 
+from Game.Level.start import select_difficulty
+
+select_difficulty(2)
+
+# Game/__init__.py
+a = 10
+
+# Game/main.py
+b = 20
+
+# start.py
+def select_difficulty(d):
+    print(d)
