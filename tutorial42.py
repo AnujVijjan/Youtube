d8c6a08e21504ba39bdf7ff2fdb110d6c5f53a57
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-07-14 20:14:28 +0530

    Create tutorial42.py

---
+++ tutorial42.py
@@ -0,0 +1,19 @@
+class Student:
+    # student_name = "anuj"
+
+    # def greet(self):
+    #     print("Hello")
+    pass
+a = 10
+
+anuj = Student() # Object
+
+# print(Student.student_name)
+
+# print(anuj.student_name)
+
+
+# anuj.greet()
+# print(type(anuj))
+
+# print(type(a))
