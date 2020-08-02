68ed9d6602fb28e3711371a318fffe3d1302c903
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-08-02 21:13:24 +0530

    Create cpp_tutorial4.cpp

---
+++ cpp_tutorial4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // This is C++ program
+    // hgifgoifgo
+    /* hiosdfjofgodf 
+        fdghfjgfdjbgdjfg
+        gfdkjgfdkjgjdfk
+    */
+   
+    int age = 10;
+    float per;
+
+    per = 90.10;
+
+    cout<<"Age: "<<age;
+
+    cout<<"Percentage: "<<per;
+    return 10;
+}
