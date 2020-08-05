f5680c567b329fd40eb5ff6d3f4baaca6eeaa22b
AnujVijjan <49342204+AnujVijjan@users.noreply.github.com>
2020-08-05 20:28:42 +0530

    Create cpp_tutorial5.cpp

---
+++ cpp_tutorial5.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    char c = 'a';
+
+    float f = 10;
+
+    bool b = false;
+
+    double d = 10.10;
+
+    short s = 2;
+
+    long l = 1000000;
+    
+    // cout<<"a = "<<a;
+
+    // cout<<"c = "<<c;
+
+    // cout<<"f = "<<f;
+
+    // cout<<"b = "<<b;
+
+    cout<<"d = "<<d;
+
+    cout<<"s = "<<s;
+
+    cout<<"l = "<<l;
+    return 0;
+}
